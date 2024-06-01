b9fc49cbf8d829f8268e63b666137fa0b532c1a1
RuiXuan_Zhu <U202215337@hust.edu.cn>
2024-06-01 12:20:25 +0800

    完成map

--- lab1_MapReduce/src/main.py
+++ lab1_MapReduce/src/main.py
@@ -1,28 +1,14 @@
 import os
+import time
 import threading
 from map import create_map_threads
 from combine import combine_function
 from shuffle import shuffle_function
 from reduce import reduce_function, output_results
 
-words_set = set(open('../data/words.txt', 'r').read().split())
-map_output_file = '../tmp/map_output'
-map_threads = create_map_threads(words_set, map_output_file)
-for t in map_threads:
-    t.join()
-'''
-with open(map_output_file, 'r', encoding='utf-8') as in_file:
-    lines = in_file.readlines()
-word_counts = {}
-for line in lines:
-    parts = line.strip().split(',')
-    if len(parts) == 3:  # 确保每一行有三个部分
-        word, count = parts
-        word_counts[word] = int(count)
-
-combined_results = [combine_function(result) for result in map_results]
-shuffled_data = shuffle_function(combined_results)
-reduced_result = reduce_function(shuffled_data)
-top_1000_words = sorted(reduced_result.items(), key=lambda x: x[1], reverse=True)[:1000]
-output_results(top_1000_words)
-'''
+if __name__ == "__main__":
+    words_set = set(open('../data/words.txt', 'r').read().split())
+    map_output = '../tmp/map_output'
+    map_threads = create_map_threads(words_set, map_output)
+    for t in map_threads:
+        t.join()
--- lab1_MapReduce/src/map.py
+++ lab1_MapReduce/src/map.py
@@ -10,26 +10,19 @@ def extract_words_from_file(file_path):
         words = re.findall(r'\b[a-zA-Z]+\b', content)
         return words
 
-def map_function(folder_index, words_set, output_file):
+def map_function(folder_index, words_set, output_path):
     word_counts = defaultdict(int)
     folder_path = os.path.join('../data', f'folder_{folder_index}')
     for filename in os.listdir(folder_path):
         file_path = os.path.join(folder_path, filename)
         if os.path.isfile(file_path):
             content = extract_words_from_file(file_path)
-            output_path = os.path.join(output_file, f'{filename}')
-            output_path = output_path.replace('\\', '/')
-            with open(output_path, 'w', encoding='utf-8') as output:
-                for item in content:
-                    output.write(item)
-                    output.write(' ')
-            '''for word in words_set:
-                count = content.count(word)
-                word_counts[(filename, word)] = count
-
-    with open(output_file, 'a', encoding='utf-8') as out_file:
-        for (filename, word), count in word_counts.items():
-            out_file.write(f"{filename},{word},{count}\n")'''
+            output_file = os.path.join(output_path, f'map_{folder_index}.txt')
+            #output_file = output_path.replace('\\', '/')
+            with open(output_file, 'a', encoding='utf-8') as output:
+                for word in content:
+                    if word in words_set:
+                        output.write("{},{}\n".format(word, 1))        
 
 def create_map_threads(words_set, output_file):
     threads = []
