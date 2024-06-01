4c83cc50a381f80bb59751e56bcc3c99336095a8
RuiXuan_Zhu <U202215337@hust.edu.cn>
2024-06-01 14:23:47 +0800

    取消map_output在main.py中的路径，直接写在map+function中

--- lab1_MapReduce/src/main.py
+++ lab1_MapReduce/src/main.py
@@ -7,8 +7,12 @@ from shuffle import shuffle_function
 from reduce import reduce_function, output_results
 
 if __name__ == "__main__":
+    start_time = time.time()
+
     words_set = set(open('../data/words.txt', 'r').read().split())
-    map_output = '../tmp/map_output'
-    map_threads = create_map_threads(words_set, map_output)
+    map_threads = create_map_threads(words_set)
     for t in map_threads:
         t.join()
+    
+    finish_time = time.time()
+    print("MapReduce time: %.3f s." % (finish_time - start_time))
--- lab1_MapReduce/src/map.py
+++ lab1_MapReduce/src/map.py
@@ -10,24 +10,23 @@ def extract_words_from_file(file_path):
         words = re.findall(r'\b[a-zA-Z]+\b', content)
         return words
 
-def map_function(folder_index, words_set, output_path):
+def map_function(folder_index, words_set):
     word_counts = defaultdict(int)
     folder_path = os.path.join('../data', f'folder_{folder_index}')
     for filename in os.listdir(folder_path):
         file_path = os.path.join(folder_path, filename)
         if os.path.isfile(file_path):
             content = extract_words_from_file(file_path)
-            output_file = os.path.join(output_path, f'map_{folder_index}.txt')
-            #output_file = output_path.replace('\\', '/')
+            output_file = os.path.join('../tmp/map_output', f'map_{folder_index}.txt')
             with open(output_file, 'a', encoding='utf-8') as output:
                 for word in content:
                     if word in words_set:
                         output.write("{},{}\n".format(word, 1))        
 
-def create_map_threads(words_set, output_file):
+def create_map_threads(words_set):
     threads = []
     for i in range(1, 10):
-        t = threading.Thread(target=map_function, args=(i, words_set, output_file))
+        t = threading.Thread(target=map_function, args=(i, words_set))
         threads.append(t)
         t.start()
     return threads
